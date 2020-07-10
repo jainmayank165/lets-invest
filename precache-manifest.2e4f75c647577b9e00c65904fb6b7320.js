7645d96be3eb78c493bfe282cedc2ca637bdea32
jainmayank165 <jainmayank165@gmail.com>
2020-07-10 23:56:16 +0530

    Updates

---
+++ precache-manifest.2e4f75c647577b9e00c65904fb6b7320.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be2c24812d59e36c3d98b648a5b6561",
+    "url": "/lets-invest/index.html"
+  },
+  {
+    "revision": "0c511efe587fa151fa86",
+    "url": "/lets-invest/static/css/2.eae57d2a.chunk.css"
+  },
+  {
+    "revision": "81f555f8133a60683104",
+    "url": "/lets-invest/static/css/main.436b61a0.chunk.css"
+  },
+  {
+    "revision": "0c511efe587fa151fa86",
+    "url": "/lets-invest/static/js/2.11c3b2e7.chunk.js"
+  },
+  {
+    "revision": "0a945029ff83092ea7339b9dfdac8f69",
+    "url": "/lets-invest/static/js/2.11c3b2e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f555f8133a60683104",
+    "url": "/lets-invest/static/js/main.11c448c3.chunk.js"
+  },
+  {
+    "revision": "f3a5b1f01f4f249d36b1",
+    "url": "/lets-invest/static/js/runtime-main.02162cbf.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/lets-invest/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/lets-invest/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/lets-invest/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/lets-invest/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/lets-invest/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/lets-invest/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/lets-invest/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/lets-invest/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/lets-invest/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/lets-invest/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/lets-invest/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/lets-invest/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/lets-invest/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/lets-invest/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/lets-invest/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/lets-invest/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/lets-invest/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "fd5abe514a873396501e6b58a4d405dd",
+    "url": "/lets-invest/static/media/stock3.fd5abe51.jpg"
+  },
+  {
+    "revision": "2cee3ad65d7c33a203d75605183c7981",
+    "url": "/lets-invest/static/media/stock4.2cee3ad6.jpg"
+  },
+  {
+    "revision": "e9e8e605a208e1905d399fa79c573197",
+    "url": "/lets-invest/static/media/stock5.e9e8e605.jpg"
+  }
+]);
